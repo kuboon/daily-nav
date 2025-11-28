97720ed325f3cf402f77e11ccb2d03db4b98517e
Ohkubo KOHEI <o@kbn.one>
2025-11-28 00:36:07 +0000

    apply deno fmt, 1.0.2

--- README.md
+++ README.md
@@ -3,12 +3,19 @@
 [Install via BRAT](obsidian://brat?plugin=https://github.com/kuboon/daily-nav)
 
 ## Features
+
 This plugin adds two commands to the command palette:
 
 - `Daily Nav: Open Yesterday's Note`
 - `Daily Nav: Open Tomorrow's Note`
 
-It is recommended to use with [Note Toolbar](https://github.com/chrisgurney/obsidian-note-toolbar)
+It is recommended to use with
+[Note Toolbar](https://github.com/chrisgurney/obsidian-note-toolbar)
 
 ## How it works
-This plugin depends on [obsidian-daily-notes-interface](https://github.com/liamcain/obsidian-daily-notes-interface) which reads settings from the daily notes plugin and [periodic notes](https://github.com/liamcain/obsidian-periodic-notes) plugin if you are using.
+
+This plugin depends on
+[obsidian-daily-notes-interface](https://github.com/liamcain/obsidian-daily-notes-interface)
+which reads settings from the daily notes plugin and
+[periodic notes](https://github.com/liamcain/obsidian-periodic-notes) plugin if
+you are using.
--- main.ts
+++ main.ts
@@ -1,36 +1,65 @@
-import { Plugin } from 'obsidian';
+import { Plugin } from "obsidian";
 import { getAllDailyNotes } from "obsidian-daily-notes-interface";
 
 export default class DailyNav extends Plugin {
-	prepare() {
-		const allDailyNotes = getAllDailyNotes();
-		const allKeys = Object.keys(allDailyNotes).sort();
-		const activeFile = this.app.workspace.getActiveFile()!;
-		const activeNoteIdx = allKeys.findIndex(key => allDailyNotes[key]!.name === activeFile.name)!;
-		return { allDailyNotes, allKeys, activeNoteIdx }
-	}
-	override onload() {
-		this.addCommand({
-			id: 'open-yesterdays-note',
-			name: 'Open yesterday\'s note',
-			callback: () => {
-				const { allDailyNotes, allKeys, activeNoteIdx } = this.prepare();
-				const yesterdayNote = allDailyNotes[allKeys[activeNoteIdx - 1]];
-				if(yesterdayNote) {
-					this.app.workspace.openLinkText(yesterdayNote.basename, '');
-				}
-			}
-		});
-		this.addCommand({
-			id: 'open-tomorrows-note',
-			name: 'Open tomorrow\'s note',
-			callback: () => {
-				const { allDailyNotes, allKeys, activeNoteIdx } = this.prepare();
-				const tomorrowNote = allDailyNotes[allKeys[activeNoteIdx + 1]];
-				if(tomorrowNote) {
-					this.app.workspace.openLinkText(tomorrowNote.basename, '');
-				}
-			}
-		});
-	}
+  prepare() {
+    const allDailyNotes = getAllDailyNotes();
+    const allKeys = Object.keys(allDailyNotes).sort();
+    const activeFile = this.app.workspace.getActiveFile()!;
+    const activeNoteIdx = allKeys.findIndex((key) =>
+      allDailyNotes[key]!.name === activeFile.name
+    )!;
+    return { allDailyNotes, allKeys, activeNoteIdx };
+  }
+  override onload() {
+    this.addCommand({
+      id: "open-yesterdays-note",
+      name: "Open yesterday's note",
+      callback: () => {
+        const { allDailyNotes, allKeys, activeNoteIdx } = this.prepare();
+        const yesterdayNote = allDailyNotes[allKeys[activeNoteIdx - 1]];
+        if (yesterdayNote) {
+          this.app.workspace.openLinkText(yesterdayNote.basename, "");
+        }
+      },
+    });
+    this.addCommand({
+      id: "open-tomorrows-note",
+      name: "Open tomorrow's note",
+      callback: () => {
+        const { allDailyNotes, allKeys, activeNoteIdx } = this.prepare();
+        const tomorrowNote = allDailyNotes[allKeys[activeNoteIdx + 1]];
+        if (tomorrowNote) {
+          this.app.workspace.openLinkText(tomorrowNote.basename, "");
+        }
+      },
+    });
+    function openSiblingNote(offset: number) {
+      const explorer = this.app.workspace.getLeavesOfType("file-explorer")[0];
+      const currentPath = app.workspace.getActiveFile().path;
+      const filePaths = Object.keys(explorer.view.fileItems);
+      const currentIdx = filePaths.indexOf(currentPath);
+      const siblingIdx = currentIdx + offset;
+      const siblingFilePath = filePaths[siblingIdx];
+      if (siblingFilePath) {
+        this.app.workspace.getLeaf().openFile(
+          this.app.vault.getAbstractFileByPath(siblingFilePath)!,
+        );
+      }
+    }
+    this.addCommand({
+      id: "open-prev-note",
+      name: "Open previous note on explorer view order",
+      callback: () => {
+        openSiblingNote(-1);
+      },
+    });
+    this.addCommand({
+      id: "open-next-note",
+      name: "Open next note on explorer view order",
+      callback: () => {
+        openSiblingNote(1);
+      },
+    });
+  }
 }
